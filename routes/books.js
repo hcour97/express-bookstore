6f9c6804105ab328ebbf2489d8c567888da73663
Hannah <hcour@gmail.com>
2024-01-02 08:28:08 -0500

    schema for new book and update book

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const Book = require("../models/book");
+const ExpressError = require("../expressError")
 
 const router = new express.Router();
+
+const jsonschema = require("jsonschema");
+const bookSchemaNew = require("../schema/bookSchemaNew.json");
+const bookSchemaUpdate = require("../schema/bookSchemaUpdate.json");
 
 
 /** GET / => {books: [book, ...]}  */
@@ -30,10 +35,19 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, bookSchemaNew);
+    if (!result.valid) {
+      return next ({
+        status: 400,
+        error: result.errors.map(e => e.stack)
+      });
+    }
     const book = await Book.create(req.body);
-    return res.status(201).json({ book });
-  } catch (err) {
-    return next(err);
+    console.log("success");
+    return res.status(201).json({book});
+    // return res.json("SUCCESS!!")
+  } catch (error) {
+    return next(error);
   }
 });
 
@@ -41,6 +55,19 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    if ("isbn" in req.body) {
+      return next({
+        status: 400,
+        message: "not allowed"
+      });
+    }
+    const result = jsonschema.validate(req.body, bookSchemaUpdate);
+    if (!result.valid) {
+      return next ({
+        status: 400,
+        errors: result.errors.map(e => e.stack)
+      });
+    } 
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
